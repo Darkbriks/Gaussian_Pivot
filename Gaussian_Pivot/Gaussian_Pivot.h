0f0849cead8ca6281c334963039e0cefa78bc5a1
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-03-09 15:36:51 +0100

    Init Commit

---
+++ Gaussian_Pivot/Gaussian_Pivot.h
@@ -0,0 +1,11 @@
+﻿#ifndef GAUSSIAN_PIVOT
+#define GAUSSIAN_PIVOT
+
+#include <map>
+#include <string>
+#include <vector>
+
+// Gaussian pivot using multiplication between lines
+std::vector<std::map<std::string, long double>> gaussian_pivot(std::vector<std::map<std::string, long double>> system, int nb_equations);
+
+#endif
