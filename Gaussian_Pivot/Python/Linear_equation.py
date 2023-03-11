81e92f8369d6b752bec2a0dace5fbb76702219eb
Darkbriks62 <104578594+Darkbriks@users.noreply.github.com>
2023-03-11 10:08:24 +0100

    Add python and web in projet

---
+++ Gaussian_Pivot/Python/Linear_equation.py
@@ -0,0 +1,8 @@
+﻿class LinearEquation:
+    def __init__(self, dict, nb_var):
+        self.equation = dict
+        self.nb_var = nb_var
+
+    def __str__(self):
+        for i in self.equation.keys():
+            print(i, self.equation[i], " +", end=" ")
---
+++ Gaussian_Pivot/Python/Main.py
@@ -0,0 +1,4 @@
+from Linear_equation import LinearEquation
+
+e = LinearEquation({}, 0)
+print(e)
